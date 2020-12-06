396ff90aa19a3e7882df8ca41b25cec3b66863e9
karthik20066002 <karthik20066002@gmail.com>
2020-12-06 12:18:35 +0530

    added the ground to place slingshot

--- sketch.js
+++ sketch.js
@@ -16,7 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground1 = new Ground(600,height,1200,20)
+    ground2 = new Ground(150,310,350,174)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -45,7 +46,8 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
-    ground.display();
+    ground1.display();
+    ground2.display();
     pig1.display();
     log1.display();
 
